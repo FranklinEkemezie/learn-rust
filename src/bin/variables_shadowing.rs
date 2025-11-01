8b97fb7286b078702639d4136f1547433b062c7e
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-01 14:06:19 +0100

    fix: variables - solve 'shadowing' practice code example

--- src/bin/variables_shadowing.rs
+++ src/bin/variables_shadowing.rs
@@ -4,11 +4,11 @@ fn main() {
     let x: i32 = 5;
     {
         let x = 12;
-        assert_eq!(x, 5)
+        assert_eq!(x, 12);
     }
 
-    assert_eq!(x, 12);
+    assert_eq!(x, 5);
 
-    let x = 42;
+    let x: i32 = 42;
     println!("{}", x);
 }
