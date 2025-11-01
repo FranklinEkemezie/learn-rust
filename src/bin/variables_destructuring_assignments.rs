d737288d0bceaa3ad765e46e73e85169ec93dc5c
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-01 15:07:37 +0100

    feat: variables - add 'destructuring assignments' practice code example

---
+++ src/bin/variables_destructuring_assignments.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    let (x, y);
+
+    (x, ..) = (3, 4);
+    [.., y] = [1, 2];
+
+    // fill in the blank to make the code work
+    assert_eq!([x, y], __);
+
+    println!("Success!");
+}
