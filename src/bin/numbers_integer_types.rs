6ba245f1f160d7c05375b014d705dbfdf1b97e2e
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:09:14 +0100

    feat: numbers - add 'integer types' practice code example

---
+++ src/bin/numbers_integer_types.rs
@@ -0,0 +1,16 @@
+#[allow(unused_variables)]
+
+// Goal: Modify `assert_eq!` to make it work
+
+fn main() {
+    let x = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+
+    println!("Success!");
+}
+
+// Get the type of the given variable, return the string representation of the type
+// e.g. i32, u64 etc.
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
