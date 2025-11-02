cb3a1aa7145039fe5eca5b18153e30cfd7f7beeb
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:18:43 +0100

    fix: numbers - solve 'integer types range' practice code example

--- src/bin/numbers_integer_types_range.rs
+++ src/bin/numbers_integer_types_range.rs
@@ -2,8 +2,8 @@
 // Fill in the blanks to make it work
 
 fn main() {
-    assert_eq!(i8::MAX, __);
-    assert_eq!(u8::MAX, __);
+    assert_eq!(i8::MAX, 127);
+    assert_eq!(u8::MAX, 255);
 
     println!("Success!");
 }
