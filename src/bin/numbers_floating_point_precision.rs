d6fbd7acf7c8668dc2ad88ef607f036a3ba2017f
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:54:30 +0100

    fix: numbers - solve 'floating point precision (using type casting)' practice code example

--- src/bin/numbers_floating_point_precision.rs
+++ src/bin/numbers_floating_point_precision.rs
@@ -7,7 +7,7 @@ fn main() {
     // here 0.1 + 0.2 could be equals to something like 0.300000000000000002
     // the default float type `f64` is 'too precise' for us here
     // we can solve this by specifying a type with lower precision, `f_32` for example.
-    assert!(0.1_f32 + 0.2_f32 == 0.3_f32);
+    assert!(0.1 as f32 + 0.2 as f32 == 0.3 as f32);
 
     println!("Success!");
 }
