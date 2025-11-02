81dcac8d3c69a346b0ba2f4d24e1d84a406aebfe
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 04:26:03 +0100

    feat: numbers - add 'computations' practice code example

---
+++ src/bin/numbers_computations.rs
@@ -0,0 +1,21 @@
+
+// Fill the blanks and fix the errors
+fn main() {
+
+    // Integer addition
+    assert!(1u32 + 2 == __);
+
+    // Integer subtraction
+    assert!(1i32 - 2 == __);
+    assert!(1u8 - 2 == -1);
+
+    // Integer multiplication
+    assert!(3 * 50 == __);
+
+    // Floating point division
+    assert!(9.6 / 3.2 == 3.0);
+
+    // Modulus operation
+    assert!(24 % 5 == __);
+
+}
