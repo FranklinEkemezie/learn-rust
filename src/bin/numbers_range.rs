b2428be605ec8fbeb1605eb83a703ea83680551e
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 04:05:10 +0100

    fix: numbers - solve 'range' practice code example

--- src/bin/numbers_range.rs
+++ src/bin/numbers_range.rs
@@ -3,14 +3,14 @@
 // - Modify `assert!` to make it work
 // Make `println!` output: 97 - 122
 fn main() {
-    let mut sum = 0;
+    let mut sum = 0; // 0 + -3 + -2 + -1 + 0 + 1  = -5
     for i in -3..2 {
         sum += i;
     }
 
-    assert!(sum == -3);
+    assert!(sum == -5);
 
     for c in 'a'..='z' {
-        println!("{}", c);
+        println!("{}", c as u8);
     }
 }
