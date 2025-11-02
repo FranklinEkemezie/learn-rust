41bbfe22eb6f56a50eb00d4c71eaa5134890b33f
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:03:08 +0100

    fix: numbers - solve 'type casting' practice code example

--- src/bin/numbers_type_casting.rs
+++ src/bin/numbers_type_casting.rs
@@ -1,10 +1,11 @@
+#[allow(unused_variables)]
 
 // Fill in the blank space
 
 fn main() {
 
     // NB: Here, the type 'u8' is being annotated onto the value 38
-    let v: i32 = 38_u8 as __;
+    let v: i32 = 38_u8 as i32; // use the 'as' keyword to cast from one integer type to another
 
     println!("Success!");
 }
