cd742d6284a4bfb2f0c2ac949db07d430a62a922
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:43:25 +0100

    fix: numbers - solve 'floating point' practice code example

--- src/bin/numbers_floating_point.rs
+++ src/bin/numbers_floating_point.rs
@@ -1,12 +1,13 @@
+#[allow(unused_variables)]
 
 // Fill the blank to make it work
 
 fn main() {
-    let x = 1_000.000_1;
+    let x: f64 = 1_000.000_1;
     let y: f32 = 0.12;
-    let z = 0.01_f64;
+    let z: f64 = 0.01_f64;
 
-    assert_eq!(type_of(&x), "__".to_string());
+    assert_eq!(type_of(&x), "f64".to_string());
     println!("Success!");
 
 }
