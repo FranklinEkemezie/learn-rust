fefb81a8fee1a9fddbf44e2eae544bd83e925d2e
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 03:53:40 +0100

    fix: numbers - solve 'floating point precision (using type annotation)' practice code example

--- src/bin/numbers_floating_point_precision.rs
+++ src/bin/numbers_floating_point_precision.rs
@@ -4,7 +4,10 @@
 // Make it work in two distinct ways
 
 fn main() {
-    assert!(0.1 + 0.2 == 0.3);
+    // here 0.1 + 0.2 could be equals to something like 0.300000000000000002
+    // the default float type `f64` is 'too precise' for us here
+    // we can solve this by specifying a type with lower precision, `f_32` for example.
+    assert!(0.1_f32 + 0.2_f32 == 0.3_f32);
 
     println!("Success!");
 }
