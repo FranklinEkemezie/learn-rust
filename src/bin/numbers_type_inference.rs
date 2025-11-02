7bf9d2081e58e6441f1b06714c80e7511ec86701
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-02 02:54:20 +0100

    feat: numbers - add 'type inference' practice code example

---
+++ src/bin/numbers_type_inference.rs
@@ -0,0 +1,14 @@
+// Type inference
+
+// Remove something to make it work
+fn main() {
+
+    let x: i32 = 5;
+    let mut y: u32 = 5;
+
+    y = x;
+
+    let z = 10; // type of z?
+
+    println!("Success!");
+}
