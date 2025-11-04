c43f9c1cc452b02bdcf6be49fe5d0c307be41a77
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-04 16:16:37 +0100

    fix: solve 'char size' practice example

--- src/bin/03_char_01_size.rs
+++ src/bin/03_char_01_size.rs
@@ -4,11 +4,14 @@
 use std::mem::size_of_val;
 
 fn main() {
+    // In Rust, a character type ('char') takes up 4 bytes
+    // It is big enough to hold any Unicode character
+
     let c1 = 'a';
-    assert_eq!(size_of_val(&c1), 1);
+    assert_eq!(size_of_val(&c1), 4);
 
     let c2 = 'ϕ';
-    assert_eq!(size_of_val(&c2), 3);
+    assert_eq!(size_of_val(&c2), 4);
 
     println!("Success!");
 }
