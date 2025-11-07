ea16e94445a7453e0163e59892ba973457f2d89f
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-08 00:58:46 +0100

    fix: solve 'statement and expression example' practice code problem

--- src/bin/04_statement_and_expression_02_example.rs
+++ src/bin/04_statement_and_expression_02_example.rs
@@ -5,7 +5,8 @@ fn main() {
 
     let v = {
         let mut x = 1;
-        x += 2
+        x += 2; // assignment operation does not return any value
+        x // return the value of x
     };
 
     assert_eq!(v, 3);
