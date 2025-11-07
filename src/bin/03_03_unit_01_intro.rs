11d86109fc544cfe545960800519ebbb784c087a
Ifeanyichukwu Ekemezie <franklynpeter2006@gmail.com>
2025-11-08 00:36:46 +0100

    fix: solve 'unit type intro' practice code example

--- src/bin/03_03_unit_01_intro.rs
+++ src/bin/03_03_unit_01_intro.rs
@@ -2,19 +2,27 @@
 // Make it work, don't modify `implicitly_ret_unit`!
 
 fn main() {
-    let _v: () = ();
 
-    let v = (2, 3);
-    assert_eq!(v, implicitly_ret_unit());
+    // In Rust, a unit type denoted by `()` is a type that
+    // does not hold any value. This is usually the return
+    // type of a function that does not return any value.
+    // The size of a unit type is zero.
+
+
+    let _v: () = (); // a tuple with no values
+
+    let v = (2, 3); // a tuple with two values
+
+    assert_eq!(_v, implicitly_ret_unit());
 
     println!("Success!");
 }
 
 fn implicitly_ret_unit() {
-    print!("I will return a ()");
+    println!("I will return a ()");
 }
 
 // Don't use this one
 fn explicitly_ret_unit() -> () {
-    print!("I will return a ()");
+    println!("I will return a ()");
 }
