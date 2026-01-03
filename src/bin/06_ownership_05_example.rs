41c68007fdb14d42106e8632268c098ecee140b6
Shadow Walker <franklynpeter2006@gmail.com>
2026-01-03 13:53:31 +0100

    fix: solve 'ownership' practice code example 5.

--- src/bin/06_ownership_05_example.rs
+++ src/bin/06_ownership_05_example.rs
@@ -1,8 +1,9 @@
 
 // Don't use clone, use copy instead
 fn main() {
-    let x = (1, 2, (), "hello".to_string());
-    let y = x.clone();
+    // Change the values in the tuple 'x' to types of known fixed size
+    let x: (i32, i32, (), &str) = (1, 2, (), "hello");
+    let y = x;
 
     println!("{:?}, {:?}", x, y);
 
