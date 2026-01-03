35b825239a1c1b0faceefed910b19beea2ddc2a8
Shadow Walker <franklynpeter2006@gmail.com>
2026-01-03 13:38:09 +0100

    fix: solve 'ownership 3' practice code example.

--- src/bin/06_onwership_03_example.rs
+++ src/bin/06_onwership_03_example.rs
@@ -8,6 +8,7 @@ fn main() {
 fn give_ownership() -> String {
     let s = String::from("hello, world");
     // Convert String to Vec
-    let _s = s.into_bytes();
+    let _s = s.as_bytes();
+
     s
 }
