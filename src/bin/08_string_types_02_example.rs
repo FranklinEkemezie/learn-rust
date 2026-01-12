1349c00857a5ac762cfc3152997eda8baf521a48
Shadow Walker <franklynpeter2006@gmail.com>
2026-01-12 18:36:56 +0100

    fix: solve 'string type 2' practice code example.

--- src/bin/08_string_types_02_example.rs
+++ src/bin/08_string_types_02_example.rs
@@ -1,8 +1,12 @@
 
 fn main() {
 
+    // Another solution is to remove the `.into()` call
+    // and the `Box` type so that `s` has the default `&str` type
+    // In this case, we won't have to pass a reference of `s` to
+    // `greetings` function since the type matches.
     let s: Box<str> = "hello, world".into();
-    greetings(s)
+    greetings(&s)
 }
 
 fn greetings(s: &str) {
