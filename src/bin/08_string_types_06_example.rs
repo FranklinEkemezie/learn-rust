80f390fe48c1893bd7b1680356ab2a0f8c4053f2
Shadow Walker <franklynpeter2006@gmail.com>
2026-01-13 22:49:04 +0100

    fix: solve 'string type 6' practice code example.

--- src/bin/08_string_types_06_example.rs
+++ src/bin/08_string_types_06_example.rs
@@ -4,7 +4,10 @@ fn main() {
 
     let s1 = String::from("hello,");
     let s2 = String::from("world!");
-    let s3 = s1 + s2;
+
+    // we can also add a reference to `s2`, `&s2` instead, and
+    // `&s2` will be inferred tas `String`
+    let s3: String = s1 + s2.as_str();
     assert_eq!(s3, "hello,world!");
-    println!("{}", s1);
+    println!("{}", s3);
 }
